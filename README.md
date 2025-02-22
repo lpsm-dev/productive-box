e8d886f29369d8a32bd5117094bdd7967e2b2547
lpsm-dev-github <lpsm-dev@protonmail.com>
2025-02-22 17:28:15 -0300

    chore: initial commit - include config files

---
+++ README.md
@@ -0,0 +1,119 @@
+<!-- BEGIN_DOCS -->
+<div align="center">
+
+<a name="readme-top"></a>
+
+<img src="https://user-images.githubusercontent.com/25841814/79395484-5081ae80-7fac-11ea-9e27-ac91472e31dd.png" width="500">
+
+Hello Human 👽! Bem-vindo ao meu repositório 👋
+
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](https://www.conventionalcommits.org/en/v1.0.0/)
+[![Semantic Release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://semantic-release.gitbook.io/semantic-release/usage/configuration)
+[![Built with Devbox](https://jetpack.io/img/devbox/shield_galaxy.svg)](https://jetpack.io/devbox/docs/contributor-quickstart/)
+
+<p>
+  Você é diurno 🐤 ou noturno 🦉?
+  <br/>
+  Vamos dar uma olhada 🧐!
+  <br/>
+  Inspirado pelo projeto [productive-box](https://github.com/maxam2017/productive-box) do @maxam2017.
+</p>
+
+</div>
+
+# Sumário
+
+<details>
+  <summary><strong>Expandir</strong></summary>
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+
+- [Overview](#overview)
+- [Setup](#setup)
+  - [Prep work](#prep-work)
+  - [Project setup](#project-setup)
+- [Referências](#refer%C3%AAncias)
+- [Contribuição](#contribui%C3%A7%C3%A3o)
+- [Versionamento](#versionamento)
+- [Troubleshooting](#troubleshooting)
+- [Show your support](#show-your-support)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+</details>
+
+# Overview
+
+Esse projeto usa a API graphQL do GitHub para obter os históricos de commits de um usuário e colocar de forma customizada em um Gist do GitHub.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+# Setup
+
+## Prep work
+
+1. Crie um novo GitHub Gist público (https://gist.github.com/)
+1. Crie um token com o escopo `gist` e `repo` e copie-o. (https://github.com/settings/tokens/new)
+   > - habilitar o escopo `repo` parece **PERIGOSO**<br/>
+   > - mas essa ação do GitHub só acessa o registro de data e hora do commit no repositório com o qual você contribuiu.
+
+## Project setup
+
+1. Faça um fork desse repositório.
+1. Habilite as "Actions" (Ações) de sua bifurcação e clique no botão "enable" (ativar).
+1. Edite as variáveis utilizadas na pipeline `.github/workflows/schedule.yml`:
+   - **GIST_ID:** A parte do ID da url do seu gist.
+   - **TIMEZONE:** O fuso horário de sua localização, por exemplo, `Asia/Taipei` para Taiwan, `America/New_York` para a América em Nova York, etc.
+1. Acesse o repositório **Settings > Secrets**
+1. Adicione as seguintes variáveis de ambiente:
+   - **GH_TOKEN:** O token do GitHub gerado acima.
+1. [Fixe o Gist recém-criado](https://help.github.com/en/github/setting-up-and-managing-your-github-profile/pinning-items-to-your-profile)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+# Referências
+
+Links relevantes para esse projeto:
+
+- https://github.com/techinpark/productive-box
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+# Contribuição
+
+Gostaria de contribuir? Isso é ótimo! Temos um guia de contribuição para te ajudar. Clique [aqui](CONTRIBUTING.md) para lê-lo.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+# Versionamento
+
+Para verificar o histórico de mudanças do projeto, acesse o arquivo [**CHANGELOG.md**](CHANGELOG.md).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+# Troubleshooting
+
+Se você tiver algum problema, [abra uma issue nesse projeto](https://github.com/homelabsz/helm-charts/issues).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+# Show your support
+
+<div align="center">
+
+Dê uma ⭐️ para esse projeto se ele te ajudou!
+
+<img alt="gif-footer" src="https://github.com/lpsm-dev/lpsm-dev/blob/main/.github/assets/yoda.gif" width="225"/>
+
+<br>
+<br>
+
+Feito com 💜 por [mim](https://github.com/lpsm-dev) :wave: inspirado no [readme-md-generator](https://github.com/kefranabg/readme-md-generator)
+
+</div>
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+<!-- END_DOCS -->
---
+++ src/index.ts
@@ -0,0 +1,186 @@
+import { resolve } from "path";
+import { config } from "dotenv";
+import { Octokit } from "@octokit/rest";
+import githubQuery from "./githubQuery";
+import generateBarChart from "./generateBarChart";
+import {
+  userInfoQuery,
+  createContributedRepoQuery,
+  createCommittedDateQuery,
+} from "./queries";
+
+config({ path: resolve(__dirname, "../.env") });
+
+interface IRepo {
+  name: string;
+  owner: string;
+}
+
+interface IRepoInfo {
+  isFork: boolean;
+  name: string;
+  owner: {
+    login: string;
+  };
+}
+
+interface IRepoResponse {
+  data: {
+    user: {
+      repositoriesContributedTo: {
+        nodes: IRepoInfo[];
+      };
+    };
+  };
+}
+
+interface ICommitEdge {
+  node: {
+    committedDate: string;
+  };
+}
+
+interface ICommitHistoryResponse {
+  data: {
+    repository: {
+      ref: {
+        target: {
+          history: {
+            edges: ICommitEdge[];
+          };
+        };
+      };
+    };
+  };
+}
+
+(async () => {
+  const userResponse = await githubQuery(userInfoQuery).catch((error) => {
+    console.error(`Unable to get username and id\n${error}`);
+    return null;
+  });
+  if (!userResponse || !userResponse.data || !userResponse.data.viewer) {
+    console.error("Invalid user response");
+    return;
+  }
+
+  const { login: username, id } = userResponse.data.viewer;
+  const contributedRepoQuery = createContributedRepoQuery(username);
+  const repoResponse: IRepoResponse = await githubQuery(
+    contributedRepoQuery,
+  ).catch((error) => {
+    console.error(`Unable to get the contributed repo\n${error}`);
+    return null;
+  });
+  if (
+    !repoResponse ||
+    !repoResponse.data ||
+    !repoResponse.data.user ||
+    !repoResponse.data.user.repositoriesContributedTo
+  ) {
+    console.error("Invalid repo response");
+    return;
+  }
+
+  const repos: IRepo[] = repoResponse.data.user.repositoriesContributedTo.nodes
+    .filter((repoInfo: IRepoInfo) => !repoInfo?.isFork)
+    .map((repoInfo: IRepoInfo) => ({
+      name: repoInfo?.name || "",
+      owner: repoInfo?.owner?.login || "",
+    }));
+
+  let committedTimeResponseMap: ICommitHistoryResponse[] = [];
+  try {
+    committedTimeResponseMap = await Promise.all(
+      repos.map(({ name, owner }) =>
+        githubQuery(createCommittedDateQuery(id, name, owner)),
+      ),
+    );
+  } catch (error) {
+    console.error(`Unable to get the commit info\n${error}`);
+    return;
+  }
+
+  let morning = 0; // 6 - 12
+  let daytime = 0; // 12 - 18
+  let evening = 0; // 18 - 24
+  let night = 0; // 0 - 6
+
+  committedTimeResponseMap.forEach((committedTimeResponse) => {
+    committedTimeResponse?.data?.repository?.ref?.target?.history?.edges.forEach(
+      (edge: ICommitEdge) => {
+        const committedDate = edge?.node?.committedDate;
+        const timeString = new Date(committedDate).toLocaleTimeString("en-US", {
+          hour12: false,
+          timeZone: process.env.TIMEZONE,
+        });
+        const hour = +timeString.split(":")[0];
+        if (hour >= 6 && hour < 12) morning++;
+        if (hour >= 12 && hour < 18) daytime++;
+        if (hour >= 18 && hour < 24) evening++;
+        if (hour >= 0 && hour < 6) night++;
+      },
+    );
+  });
+
+  const sum = morning + daytime + evening + night;
+  if (!sum) return;
+
+  const oneDay = [
+    { label: "🌞 Morning", commits: morning },
+    { label: "🌆 Daytime", commits: daytime },
+    { label: "🌃 Evening", commits: evening },
+    { label: "🌙 Night", commits: night },
+  ];
+  const lines = oneDay.reduce<string[]>((prev, cur) => {
+    const percent = (cur.commits / sum) * 100;
+    const line = [
+      `${cur.label}`.padEnd(10),
+      `${cur.commits.toString().padStart(5)} commits`.padEnd(14),
+      generateBarChart(percent, 21),
+      String(percent.toFixed(1)).padStart(5) + "%",
+    ];
+
+    return [...prev, line.join(" ")];
+  }, []);
+  const GH_TOKEN = process.env.GH_TOKEN;
+  if (!GH_TOKEN) {
+    throw new Error("GH_TOKEN environment variable is not set");
+  }
+  const GIST_ID = process.env.GIST_ID;
+  if (!GIST_ID) {
+    throw new Error("GIST_ID environment variable is not set");
+  }
+  const octokit = new Octokit({ auth: `token ${GH_TOKEN}` });
+  console.log(`GIST_ID: ${process.env.GIST_ID}`);
+  const gist = await octokit.gists
+    .get({
+      gist_id: process.env.GIST_ID as string,
+    })
+    .catch((error) => {
+      console.error(`Unable to update gist\n${error}`);
+      return null;
+    });
+  if (!gist || !gist.data || !gist.data.files) {
+    console.error("Invalid gist response");
+    return;
+  }
+
+  const filenames = Object.keys(gist.data.files);
+  if (filenames.length === 0) {
+    console.error("No files found in the gist");
+    return;
+  }
+  const filename = filenames[0];
+
+  await octokit.gists.update({
+    gist_id: GIST_ID,
+    files: {
+      [filename]: {
+        filename: filename,
+        content: lines.join("\n"),
+      },
+    },
+  });
+  console.log("✨  Done");
+})();
